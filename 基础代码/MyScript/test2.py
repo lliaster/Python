6852e3ff6998535adbf6d9ca7cbdc04eb6cd89a4
Iliaster <1286097559@qq.com>
2024-10-28 17:55:22 +0800

    20241028_1

---
+++ 基础代码/MyScript/test2.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# @Time     : 2024/10/28 16:39
+# @Author   : Lliaster
+# @File     : test2.py
+
+# print(list(range(1)))
+
+
+a = '1234'
+# b = a[3:None:-1]
+# print(b)
+
+
+b = a [0:2]
+c = a [1:None:-1]
+print(b,c)
